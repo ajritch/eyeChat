90dee60cedcd98a4f5ea3a1c72eab57beb38bc50
Annie Ritch <ritchaj@gmail.com>
2016-08-31 15:52:08 -0700

    chat is implemented

--- client/js/controllers/chatController.js
+++ client/js/controllers/chatController.js
@@ -6,51 +6,66 @@ app.controller('chatController', function($scope, $location, socketFactory, user
 	//return to login page if no username
 	if ($scope.username == '') {
 		$location.path('/');
-	}
-
-	//re-establish the looping!
-	highlight_row = undefined;
-    highlight_cell = undefined;
-    rowblink = true;
-    clearInterval(row_loop);
-	clearInterval(cell_loop);
-    row_loop = setInterval(loopRows, INTERVAL);
-
-
-	$scope.message = '';
-	$scope.messages = '';
-
-
-	//submit new chat
-	$scope.submit = function() {
-		console.log('submitted chat');
-		$scope.message = $('#building_word').html();
-		console.log($scope.message);
+	} else {
 		
-		//send message to server
-		socketFactory.emit('add_new_chat', {'name': $scope.username, 'chat': $scope.message});
-		
-		$scope.message = '';
-		//clear out previous building_word
-		$('#building_word').html('');
+		//tell server that a new user was added
+		socketFactory.emit('add_new_user', {'name': $scope.username});
 
-		//reestablish the looping! and un-highlight!
-		$(highlight_row).toggleClass('highlight_row');
-        $(highlight_cell).toggleClass('highlight_cell');
+		//re-establish the looping!
 		highlight_row = undefined;
 	    highlight_cell = undefined;
 	    rowblink = true;
 	    clearInterval(row_loop);
 		clearInterval(cell_loop);
 	    row_loop = setInterval(loopRows, INTERVAL);
+
+
+		$scope.message = '';
+		$scope.messages = [];
+
+
+		//submit new chat
+		$scope.submit = function() {
+			$scope.message = $('#building_word').html();
+			// console.log($scope.message);
+			
+			//send message to server
+			console.log('SUBMITTING');
+			socketFactory.emit('add_new_chat', {'name': $scope.username, 'chat': $scope.message});
+			
+			$scope.message = '';
+			//clear out previous building_word
+			$('#building_word').html('');
+
+			//reestablish the looping! and un-highlight!
+			$(highlight_row).toggleClass('highlight_row');
+	        $(highlight_cell).toggleClass('highlight_cell');
+			highlight_row = undefined;
+		    highlight_cell = undefined;
+		    rowblink = true;
+		    clearInterval(row_loop);
+			clearInterval(cell_loop);
+		    row_loop = setInterval(loopRows, INTERVAL);
+		}
+
+		//ask for all previously existing messages
+		//this seems to be getting called WITH EACH SUBMIT
+		socketFactory.emit('give_me_messages', {});
+
+		//get all the messages
+		socketFactory.on('all_messages', function(data) {
+			//this is firing twice
+			console.log('MESSAGES', data);
+			$scope.messages = data.messages;
+		});
+
+		//get newly added message
+		// socketFactory.on('new_message', function(data) {
+		// 	console.log('new message', data);
+		// 	$scope.messages.push(data);
+		// });
 	}
 
-	//ask for all previously existing messages
-	socketFactory.emit('give_me_messages', {});
-	//get all the messages
-	socketFactory.on('all_messages', function(data) {
-		console.log('MESSAGES', data);
-		$scope.messages = data.messages;
-	});
+
 	
 });
--- server.js
+++ server.js
@@ -33,17 +33,25 @@ io.sockets.on('connection', function(socket) {
 	socket.on('add_new_user', function(data) {
 		// console.log(data.name);
 		users.push({'name': data.name, 'id': socket.id});
-		socket.emit('hello_new_user', {'name': data.name, 'messages': messages});		
+		socket.emit('hello_new_user', {'name': data.name, 'messages': messages});
+		var alert = '';
+		alert += data.name + ' has entered the chatroom.';
+		messages.push({'alert': alert});
+		io.emit('all_messages', {'messages': messages});		
 	});
 
 	//new user wants all previous messages
 	socket.on('give_me_messages', function(data) {
+		console.log('sending all', messages);
 		socket.emit('all_messages', {'messages': messages});
 	})
 
 	//a new chat has been submitted
 	socket.on('add_new_chat', function(data) {
+		console.log('NEW MESSAGE', data);
 		messages.push({'name': data.name, 'chat': data.chat});
+		console.log('ALL', messages);
+		// io.emit('new_message', {'name': data.name, 'chat': data.chat});
 		io.emit('all_messages', {'messages': messages});
 	})
 })
